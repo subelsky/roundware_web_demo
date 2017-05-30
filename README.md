478830b70aa0261337cd5e71e731e15b18c5c7f5
Mike Subelsky <mike@subelsky.com>
2017-05-30 18:00:24 -0400

    Initial commit, barely working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# roundware_web_demo
-Demonstration site for roundware-web-framework
+# Roundware Web Demo
+
+Demonstration site for [Roundware Server](https://github.com/roundware/roundware-server) and [Roundware Web Framework](https://github.com/roundware/roundware-web-framework).
+
+# Projects Used
+
+* [Sweetalert](http://t4t5.github.io/sweetalert/)
+* [Howler]
+* [Bootstrap]
+* [Initializr]
+* [JQuery]
---
+++ js/main.js
@@ -0,0 +1,63 @@
+var roundwareServerUrl = "http://roundware.dyndns.org/api/2";
+var roundwareProjectId = 10;
+var playButton = $("#playbutton");
+var pauseButton = $("#pausebutton");
+var spinner = $("#spinner");
+var tagIds = $("#tag_ids");
+
+var roundware = new Roundware({
+  serverUrl: roundwareServerUrl,
+  projectId: roundwareProjectId
+});
+
+function handleListening(streamURL) {
+  console.info("Starting to listen to " + streamURL);
+
+  var sound = new Howl({
+    src: [streamURL],
+    autoplay: true,
+    html5: true
+  });
+
+  window.sound = sound;
+
+  sound.on('play',function handlePlayStart() {
+    // roundware.play(); TODO not implemented yet
+    pauseButton.removeAttr("disabled");
+    playButton.attr("disabled","disabled");
+  });
+
+  sound.on('pause',function handlePlayPause() {
+    // roundware.pause(); TODO not implemented yet
+    pauseButton.attr("disabled","disabled");
+    playButton.removeAttr("disabled");
+  });
+
+  spinner.remove();
+  sound.play();
+
+  pauseButton.click(function pauseButtonClicked() { sound.pause(); });
+  playButton.click(function playButtonClicked() { sound.play(); });
+}
+
+function handleListeningFailure(userErrMsg,techErrMsg) {
+  console.error("Roundware Start Error",userErrMsg,techErrMsg);
+
+  // swal == sweetalert convenience function
+  swal({
+    title: "Unable to start Roundware",
+    text: userErrMsg,
+    type: "error",
+    confirmButtonText: "OK"
+  });
+}
+
+$(function startApp() {
+  roundware.start().
+    then(handleListening).
+    catch(handleListeningFailure);
+
+  tagIds.change(function(evt) {
+    roundware.tags(tagIds.val().join(","));
+  });
+});
